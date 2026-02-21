744cdff5df3cb3315e661b21616a5cf428ebcb15
Girish Kumbar <girishkumbar100@gmail.com>
2026-02-21 21:27:35 +0530

    Update settings.py

--- admin_project/settings.py
+++ admin_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin_app.apps.AdminAppConfig',
+    'admin_app.apps.AdminAppConfig'
 ]
 
 MIDDLEWARE = [
